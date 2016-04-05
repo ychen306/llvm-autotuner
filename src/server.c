8d520b39b3183ddede9c67fa02d881a2a0a24d05
Yishen Chen <cyt046@gmail.com>
2016-04-04 19:38:33 -0500

    fix forking bug

--- src/server.c
+++ src/server.c
@@ -1,8 +1,10 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <sys/socket.h>
+#include <signal.h>
 #include <sys/un.h>
 #include <fcntl.h>
 #include <dlfcn.h>
@@ -10,6 +12,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
 
 #include "common.h"
 
@@ -22,7 +25,6 @@
 
 #define OUT_FILENAME "worker-data.txt"
 #define MAXFD 256
-#define DEFAULT_MAX_CLIENT 4
 #define MAX_CLIENT
 
 extern uint32_t _server_invos[];
@@ -30,7 +32,7 @@ extern uint32_t _server_num_invos;
 
 void _server_init() __attribute__ ((constructor));
 
-int max_client; 
+int max_client;
 
 int is_parent = 1;
 
@@ -41,7 +43,7 @@ struct response {
 };
 
 static inline struct response *make_error(char *msg)
-{ 
+{
 	struct response *resp = malloc(sizeof (struct response));
 	resp->success = 0;
 	strcpy(resp->msg, msg);
@@ -49,7 +51,7 @@ static inline struct response *make_error(char *msg)
 }
 
 static inline struct response *make_report(float time_spent)
-{ 
+{
 	struct response *resp = malloc(sizeof (struct response));
 	resp->success = 1;
 	resp->elapsed = time_spent;
@@ -57,22 +59,28 @@ static inline struct response *make_report(float time_spent)
 }
 
 // send response to the client and kill current process
-static inline void *respond(int fd, struct response *resp)
-{ 
-    write(fd, resp, sizeof (struct response));
-    free(resp);
-    exit(0);
+static inline void respond(int fd, struct response *resp)
+{
+	write(fd, resp, sizeof (struct response));
+	free(resp);
+	close(fd);
+	_exit(0);
 }
 
 static inline void dump_worker_data(const char *sock_path)
-{ 
-    FILE *out_file = fopen(OUT_FILENAME, "a");
-    fprintf(out_file, "%s\n", sock_path);
+{
+	FILE *out_file = fopen(OUT_FILENAME, "a");
+	fprintf(out_file, "%s\n", sock_path);
 	fclose(out_file);
-} 
+}
+
+void handle_sigchld(int sig)
+{
+	while (waitpid((pid_t)(-1), 0, WNOHANG) > 0) {}
+}
 
 uint32_t _server_spawn_worker(uint32_t (*orig_func)(void *), char *funcname, void *args)
-{ 
+{
 	static int invo = 0;
 
 	int can_spawn = 0;
@@ -86,98 +94,104 @@ uint32_t _server_spawn_worker(uint32_t (*orig_func)(void *), char *funcname, voi
 			}
 	}
 
-    char msg[LIBPATH_MAX_LEN]; 
-    memset(msg, 0, sizeof msg);
+	char msg[LIBPATH_MAX_LEN];
+	memset(msg, 0, sizeof msg);
 
-    char sock_path[100] = "/tmp/tuning-XXXXXX";
-    if (can_spawn) {
-        mkdtemp(sock_path);
-        strcat(sock_path, "/socket");
-    }
-    
-    if (can_spawn && fork()) { // body of worker process
-        is_parent = 0;
+	char sock_path[100] = "/tmp/tuning-XXXXXX";
+	if (can_spawn) {
+		mkdtemp(sock_path);
+		strcat(sock_path, "/socket");
+	}
 
-        daemon(1, 0);
-        struct sockaddr_un addr;
-        int sockfd;
+	if (can_spawn && fork() == 0) { // body of worker process
+		is_parent = 0;
 
-        if ((sockfd=socket(AF_UNIX, SOCK_STREAM, 0)) == - 1) {
-            exit(-1);
-        }
+		daemon(1, 0);
+		struct sockaddr_un addr;
+		int sockfd;
 
-        memset(&addr, 0, sizeof (addr));
-        addr.sun_family = AF_UNIX;
-        strncpy(addr.sun_path, sock_path, (sizeof (addr.sun_path))-1);
+		if ((sockfd=socket(AF_UNIX, SOCK_STREAM, 0)) == - 1) {
+			exit(-1);
+		}
 
-        if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
-            exit(-1);
-        }
+		struct sigaction sa;
+		sa.sa_handler = &handle_sigchld;
+		sigemptyset(&sa.sa_mask);
+		sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+		if (sigaction(SIGCHLD, &sa, 0) == -1) {
+			perror(0);
+			exit(1);
+		}
 
-        if (listen(sockfd, max_client) == -1) {
-            exit(-1);
-        }
+		memset(&addr, 0, sizeof (addr));
+		addr.sun_family = AF_UNIX;
+		strncpy(addr.sun_path, sock_path, (sizeof (addr.sun_path))-1);
 
-        for (;;) {
-            int cli_fd;
-            if ((cli_fd=accept(sockfd, NULL, NULL)) == -1) {
-                continue;
-            }
+		if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+			exit(-1);
+		}
 
-            if (read(cli_fd, msg, LIBPATH_MAX_LEN) <= 0) {
+		if (listen(sockfd, max_client) == -1) {
+			exit(-1);
+		}
+
+		for (;;) {
+			int cli_fd;
+			if ((cli_fd=accept(sockfd, NULL, NULL)) == -1) {
 				continue;
 			}
 
-            // read control byte and see if needs to kill current worker
-            if (msg[0] == KILL) {
-                close(cli_fd);
-                break;
-            }
+			if (read(cli_fd, msg, LIBPATH_MAX_LEN) <= 0) {
+				continue;
+			}
 
-            if (fork()) { 
-                // lookup the function from shared library
-                void *lib = dlopen(msg, RTLD_NOW);
-                if (!lib) respond(cli_fd, make_error(CANT_LOAD_LIB));
+			// read control byte and see if needs to kill current worker
+			if (msg[0] == KILL) {
+				close(cli_fd);
+				break;
+			}
 
-                void *(*func)(void *) = dlsym(lib, funcname); 
-                if (!func) respond(cli_fd, make_error(CANT_LOAD_FUNC));
+			if (fork() == 0) {
+				close(sockfd);
 
-                // run the function
-                struct timespec begin, end;
-                clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
-                func(args); 
-                clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
-                float time_spent = (float) ((end.tv_sec-begin.tv_sec) * 1e9 +
-                        (end.tv_nsec - begin.tv_nsec));
+				// lookup the function from shared library
+				void *lib = dlopen(msg, RTLD_NOW);
+				if (!lib) {
+					respond(cli_fd, make_error(dlerror()));
+				}
 
-                respond(cli_fd, make_report(time_spent));
-            }
+				void *(*func)(void *) = dlsym(lib, funcname);
+				if (!func) {
+					respond(cli_fd, make_error(dlerror()));
+				}
 
-            memset(msg, 0, sizeof msg);
-            close(cli_fd);
-        }
-        unlink(sock_path);
-        exit(0);
-    } else { // body of parent process 
-        if (can_spawn) {
-            dump_worker_data(sock_path);
-        }
-        return orig_func(args); 
-    }
+				// run the function
+				struct timespec begin, end;
+				clock_gettime(CLOCK_MONOTONIC, &begin);
+				func(args);
+				clock_gettime(CLOCK_MONOTONIC, &end);
+				float time_spent = (float) ((end.tv_sec-begin.tv_sec) * 1e9 +
+						(end.tv_nsec - begin.tv_nsec));
+
+				respond(cli_fd, make_report(time_spent));
+
+			}
+
+			memset(msg, 0, sizeof msg);
+			close(cli_fd);
+		}
+		unlink(sock_path);
+		exit(0);
+	} else { // body of parent process
+		if (can_spawn) {
+			dump_worker_data(sock_path);
+		}
+		return orig_func(args);
+	}
 }
 
 void _server_init()
 {
-    const char *max_client_str = getenv("MAX_CLIENT");
-    if (!max_client_str) {
-        max_client = DEFAULT_MAX_CLIENT;
-    } else { 
-        max_client = atoi(max_client_str);
-        if (!max_client) {
-            fprintf(stderr, "invalid $MAX_CLIENT\n");
-            exit(-1);
-        }
-    }
-
+	max_client = sysconf(_SC_NPROCESSORS_ONLN);
 	remove(OUT_FILENAME);
 }
