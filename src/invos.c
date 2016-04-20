21fa81081f0c1f8cbb79b9b2ec9d030d16d1061e
Yishen Chen <cyt046@gmail.com>
2016-04-20 10:19:13 -0500

    formatted c files

--- src/invos.c
+++ src/invos.c
@@ -3,8 +3,8 @@
 
 #include "common.h"
 
-void _invos_init() __attribute__ ((constructor));
-void _invos_dump() __attribute__ ((destructor));
+void _invos_init() __attribute__((constructor));
+void _invos_dump() __attribute__((destructor));
 
 #define OUT_FILENAME "invocations.txt"
 
@@ -12,24 +12,14 @@ FILE *out;
 
 static __thread struct timespec begin;
 
-void _invos_begin()
-{
-	clock_gettime(CLOCK_MONOTONIC, &begin);
+void _invos_begin() { clock_gettime(CLOCK_MONOTONIC, &begin); }
+
+void _invos_end() {
+  struct timespec end;
+  clock_gettime(CLOCK_MONOTONIC, &end);
+  fprintf(out, "%f ", diff_time(&end, &begin));
 }
 
-void _invos_end()
-{
-	struct timespec end;
-	clock_gettime(CLOCK_MONOTONIC, &end);
-	fprintf(out, "%f ", diff_time(&end, &begin));
-}
+void _invos_init() { out = fopen(OUT_FILENAME, "w"); }
 
-void _invos_init()
-{
-	out = fopen(OUT_FILENAME, "w");
-}
-
-void _invos_dump()
-{
-	fclose(out);
-}
+void _invos_dump() { fclose(out); }
