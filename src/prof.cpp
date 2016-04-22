0adee4cfc158116c281e92093f24f6923d4fb426
Yishen Chen <cyt046@gmail.com>
2016-04-22 15:41:04 -0500

    port prof.c to prof.cpp and use sparse matrix

---
+++ src/prof.cpp
@@ -0,0 +1,306 @@
+#include <time.h>
+#include <sys/time.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <map>
+#include <vector>
+
+#include "common.h"
+
+#define PROF_FLAT_OUT "loop-prof.flat.csv"
+#define PROF_GRAPH_OUT "loop-prof.graph.csv"
+#define PROF_DUMP "loop_prof.out"
+
+#define END_OF_ROW -1
+
+// sample every 100 us
+#define SAMPLING_INTERVAL 100
+
+static uint32_t _prof_num_loops_tot = 0;
+
+void _prof_init() __attribute__((constructor));
+void _prof_dump() __attribute__((destructor));
+
+// Profile data about a single loop
+struct loop_data {
+  char *func;
+  int32_t header_id; // > 0 if it's loop, = 0 if it's a function
+  int64_t runs;
+};
+
+class SparseMatrix {
+  typedef std::pair<unsigned, unsigned> Coord;
+  // mapping coordinate to value
+  std::map<Coord, uint32_t> m;
+
+public:
+  uint32_t &get(unsigned x, unsigned y) { return m[Coord(x, y)]; }
+} profiles;
+
+// Create a linked list of descriptors, one per linked module.
+//
+typedef struct module_desc_t {
+  uint32_t _prof_num_loops;
+  struct loop_data *_prof_loops_p;
+  uint32_t *_prof_loops_running_p;
+  struct module_desc_t *next;
+} module_desc;
+
+module_desc *module_desc_list_head = NULL;
+module_desc *module_desc_list_tail = NULL;
+
+static module_desc *get_new_desc_list_node() {
+  module_desc *new_entry = new module_desc();
+  if (new_entry == NULL) {
+    perror("append_new_desc_list_node(): ");
+    exit(1);
+  }
+  if (module_desc_list_tail == NULL)
+    module_desc_list_head = new_entry;
+  else {
+    assert(module_desc_list_tail->next == NULL && "Incorrect list tail");
+    module_desc_list_tail->next = new_entry;
+  }
+  module_desc_list_tail = new_entry;
+  return new_entry;
+}
+
+extern "C" void add_module_desc(int32_t *_numloops, struct loop_data *_p_l,
+                                int32_t *_p_l_r) {
+  int32_t numloops = *_numloops;
+  assert(numloops >= 0 && "Unexpected negative value for _numloops");
+  _prof_num_loops_tot += (uint32_t)numloops;
+
+  module_desc *new_entry = get_new_desc_list_node();
+  new_entry->_prof_num_loops = (uint32_t)numloops;
+  new_entry->_prof_loops_p = _p_l;
+  new_entry->_prof_loops_running_p = (uint32_t *)_p_l_r;
+  new_entry->next = NULL;
+#ifndef NDEBUG
+  printf("Registering one module desc!\n");
+#endif
+}
+
+static struct timespec begin;
+
+FILE *dumpfile;
+size_t dumpsize;
+size_t num_sampled;
+
+struct fraction {
+  size_t a, b;
+};
+
+static inline float frac2num(struct fraction *frac) {
+  return ((float)frac->a) / frac->b;
+}
+
+// exponential distribution with lambda = 1
+// note that this means the expected value is also one (E[X] = 1/lambda)
+//
+// stolen from:
+// https://en.wikipedia.org/wiki/Exponential_distribution#Generating_exponential_variates
+static inline float rand_exp() {
+  float r = (float)rand() / (float)RAND_MAX, x = -logf(r);
+  return x;
+}
+
+static void dump_sample(int signo);
+
+// setup a timer that fires in T microseconds
+// T is a exponential random variable with expectation of `SAMPLING_INTERVAL`
+// microseconds
+static void setup_timer() {
+  struct itimerval timerspec;
+
+  timerspec.it_interval.tv_sec = 0;
+  timerspec.it_interval.tv_usec = 0;
+  timerspec.it_value.tv_sec = 0;
+  timerspec.it_value.tv_usec = SAMPLING_INTERVAL * (rand_exp() + 0.5);
+
+  if (signal(SIGPROF, dump_sample) == SIG_ERR) {
+    perror("Unable to catch SIGPROF");
+    exit(1);
+  }
+
+  setitimer(ITIMER_PROF, &timerspec, NULL);
+}
+
+// running_instance[] is an array of integers recording which loops were
+// running at the time of the sample.  Use those to accumulate an NxN
+// matrix of profile entries, where profiles[i][j] is for loops i and j,
+// and each profile entry is as defined above.
+//
+static void collect_sample_impl(
+    std::vector<std::pair<unsigned, unsigned>> running_instance) {
+  for (unsigned i = 0, e = running_instance.size(); i != e; i++) {
+    const auto &coli = running_instance[i];
+    profiles.get(coli.first, coli.first) += 1;
+
+    for (unsigned j = i + 1, e = running_instance.size(); j != e; j++) {
+      const auto &colj = running_instance[j];
+      if (coli.second < colj.second) {
+        // i called j
+        profiles.get(coli.first, colj.first) += 1;
+      } else {
+        // j called i
+        profiles.get(colj.first, colj.first) += 1;
+      }
+    }
+  }
+}
+
+// turn [(col1, val1), ...] into [val1, val2, ...]
+// return bytes read from `dump`
+static int uncompress_one_row(std::vector<std::pair<unsigned, unsigned>> &row,
+                              int32_t *dump) {
+  int i = 0;
+  for (;;) {
+    int32_t col = dump[i++];
+    if (col == END_OF_ROW)
+      break;
+
+    uint32_t val = dump[i++];
+    row.push_back({col, val});
+  }
+  return i;
+}
+
+static void collect_samples(int32_t *dump) {
+  // Nothing to do if there are zero loops
+  if (_prof_num_loops_tot == 0)
+    return;
+
+  std::vector<std::pair<unsigned, unsigned>> running_instance;
+
+  size_t i;
+  for (i = 0; i < num_sampled; i++) {
+
+#ifndef NDEBUG
+    // Debug infinite loop
+    static int inCollect = 0;
+    printf("In collect_sample %d\n", ++inCollect);
+#endif
+
+    // update profiles of all the loops given a new sample
+    dump += uncompress_one_row(running_instance, dump);
+    collect_sample_impl(running_instance);
+    running_instance.resize(0);
+  }
+}
+
+// similar to `fwrite` except guaranteeing writing all bytes
+static int fwriteall(void *buf, size_t size, FILE *f) {
+  size_t num_written = 0;
+  do {
+    ssize_t bytes = fwrite((char *)buf + num_written, 1, size - num_written, f);
+    assert(bytes >= 0 && "fwrite failed");
+    num_written += bytes;
+  } while (num_written < size);
+  return 0;
+}
+
+// Sample what loops and functions are running and write sample data to a file
+static void dump_one_sample(module_desc *desc, uint32_t global_idx) {
+  // Write the sample data for each module
+  uint32_t i;
+  for (i = 0; i < desc->_prof_num_loops; i++, global_idx++) {
+    if (desc->_prof_loops_running_p[i] == 0)
+      continue;
+
+    fwriteall(&global_idx, sizeof(uint32_t), dumpfile);
+    fwriteall(desc->_prof_loops_p + i, sizeof(uint32_t), dumpfile);
+    dumpsize += sizeof(uint32_t) * 2;
+  }
+}
+
+// Sample what loops and functions are running and write sample data to a file
+static void dump_sample(int signo) {
+  uint32_t global_idx = 0;
+  for (module_desc *desc = module_desc_list_head; desc != NULL;
+       desc = desc->next) {
+    dump_one_sample(desc, global_idx);
+    global_idx += desc->_prof_num_loops;
+  }
+
+  // write a token to signify end of the row
+  int end = END_OF_ROW;
+  fwriteall(&end, sizeof(uint32_t), dumpfile);
+  dumpsize += sizeof(uint32_t);
+
+  num_sampled++;
+  setup_timer();
+}
+
+void _prof_init() {
+  dumpfile = fopen(PROF_DUMP, "w+");
+
+  srand(time(NULL));
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &begin);
+  setup_timer();
+}
+
+void _prof_dump() {
+  // disarm timer
+  struct itimerval timerspec;
+  memset(&timerspec, 0, sizeof timerspec);
+  setitimer(SIGPROF, &timerspec, NULL);
+  signal(SIGPROF, SIG_IGN);
+
+  // read sample from the dump
+  fflush(dumpfile);
+  rewind(dumpfile);
+  int32_t *dump = (int32_t *)mmap(NULL, dumpsize, PROT_READ, MAP_PRIVATE,
+                                  fileno(dumpfile), 0);
+  assert(dump && "failed to mmap dumpfile");
+  fclose(dumpfile);
+  collect_samples(dump);
+
+  // time the process in ms
+  struct timespec end;
+  clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+  long long int elapsed =
+      (end.tv_sec - begin.tv_sec) * 1e3 + (end.tv_nsec - begin.tv_nsec) / 1e6;
+
+  FILE *flat_out = fopen(PROF_FLAT_OUT, "wb"),
+       *graph_out = fopen(PROF_GRAPH_OUT, "wb");
+
+  fprintf(flat_out, "function,header-id,runs,time(pct),time(ms)\n");
+  uint32_t loop_idx = 0;
+  for (module_desc *desc = module_desc_list_head; desc != NULL;
+       desc = desc->next) {
+    struct loop_data *prof_loops = desc->_prof_loops_p;
+    for (uint32_t i = 0; i < desc->_prof_num_loops; i++) {
+      struct loop_data *loop = &prof_loops[i];
+      float pct = (float)profiles.get(loop_idx, loop_idx) / num_sampled;
+      fprintf(flat_out, "%s,%d,%ld,%.4f,%.4f\n", loop->func, loop->header_id,
+              loop->runs, 100 * pct, elapsed * pct);
+      ++loop_idx;
+    }
+  }
+
+  for (size_t i = 0; i < _prof_num_loops_tot; i++) {
+    // number of times caller was run
+    float n = profiles.get(i, i);
+    for (size_t j = 0; j < _prof_num_loops_tot; j++) {
+      if (i == j)
+        fprintf(graph_out, "-1");
+      else
+        fprintf(graph_out, "%.4f", profiles.get(i, j) / n * 100);
+
+      if (j != _prof_num_loops_tot - 1)
+        fprintf(graph_out, "\t");
+    }
+    fprintf(graph_out, "\n");
+  }
+
+  fclose(flat_out);
+  fclose(graph_out);
+}
